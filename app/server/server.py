6560a63ab9848b85e63e8191c8e382c44c5deeee
hikari <mps.parsa.mps@gmail.com>
2025-01-02 20:29:51 +0330

    ignore s3

--- app/server/server.py
+++ app/server/server.py
@@ -2,7 +2,7 @@ from apps.applications.routes import router as app_router
 from apps.business.routes import router as business_router
 from apps.files.routes import download_router
 from apps.files.routes import router as files_router
-from apps.s3.routes import router as s3_router
+# from apps.s3.routes import router as s3_router
 from fastapi_mongo_base.core import app_factory
 from fastapi_mongo_base.routes import copy_router
 
@@ -33,4 +33,6 @@ app.include_router(download_router, prefix="/v1")
 app.include_router(business_router, prefix="/v1")
 app.include_router(app_router, prefix="/v1")
 app.include_router(app_router, prefix="/api/v1", include_in_schema=False)
-app.include_router(s3_router, include_in_schema=False)
+
+
+# app.include_router(s3_router, include_in_schema=False)
