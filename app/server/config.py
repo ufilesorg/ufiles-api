15d85758a2c189344aaf7140d74becdacce5a4c9
Mahdi Kiani <mahdikiany@gmail.com>
2025-02-01 01:27:12 +0000

    settings

--- app/server/config.py
+++ app/server/config.py
@@ -4,11 +4,11 @@ import dataclasses
 import os
 from pathlib import Path
 
-from fastapi_mongo_base.core.config import Settings as BaseSettings
+from fastapi_mongo_base.core import config
 
 
 @dataclasses.dataclass
-class Settings(BaseSettings):
+class Settings(config.Settings):
     """Server config settings."""
 
     base_dir: str = Path(__file__).resolve().parent.parent
